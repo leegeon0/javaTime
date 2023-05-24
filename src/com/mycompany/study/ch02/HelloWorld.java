6e8345eeda2994d88b16b3bd500f7ab08e683f9b
lgh1891 <lgh1891@naver.com>
2023-05-24 15:37:27 +0900

    println

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWorld");
+		System.out.println("이건희");
+		System.out.println("1993");
+		System.out.println("김포");
+		System.out.println("구래");
+		System.out.println("메트로타워");
+		
+		// alert나 console.log 같은 구문이랑 비슷하다.
+		
+	}
+
+}
